de7f95f0e4b508286dc5a27d68ad50219e152bfb
박민성 <qkr9566@gmail.com>
2022-05-06 23:30:08 +0900

    부트스트랩, 템플릿 상속, 질문답변등록

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+
 @RequestMapping("/answer")
 @RequiredArgsConstructor
 @Controller
@@ -19,10 +22,13 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
-        //  답변을 저장한다. 
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        } 
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
--- src/main/java/com/mysite/sbb/question/QuestuinController.java
+++ src/main/java/com/mysite/sbb/question/QuestuinController.java
@@ -5,10 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//post 요청을 처리할 수 있도록
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import lombok.RequiredArgsConstructor;
+
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+
+import com.mysite.sbb.answer.AnswerForm;
 
 
 @RequestMapping("/question")
@@ -28,11 +38,26 @@ public class QuestuinController {
 	}
 	
 	@RequestMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		
 		return "question_detail";
 	}
+	
+	@GetMapping("/create")
+	public String questionCreate(QuestionForm questionForm) {
+		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; // 저장 후 리스트로 화면전환
+    }
+	
 
 }
